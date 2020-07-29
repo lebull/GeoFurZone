ed8a2fcb4c438d73a9874fc18cb1d04753047ecd
lebull <bulldogboy912000@yahoo.com>
2020-07-29 13:39:34 -0500

    Gettin' 'er set up

---
+++ src/data/cons.js
@@ -0,0 +1,20 @@
+export default [
+    {
+        "name": "Midwest FurFest",
+        "location": {
+            "text": "Rosemont, Illinois",
+            "latlong": "41.979655, -87.860931",
+        },
+        "size": "large",
+        "image": "https://en.wikifur.com/w/images/a/a9/MFF-2015-Banner.png"
+    },
+    {
+        "name": "Furry Fiesta",
+        "location": {
+            "text": "Dallas, TX",
+            "latlong": "32.775812, -96.809319",
+        },
+        "size": "large",
+        "image": "https://en.wikifur.com/w/images/a/a9/MFF-2015-Banner.png"
+    }
+]
---
+++ src/data/keys.js
@@ -0,0 +1,3 @@
+export default {
+    "bingmaps": "AiLO5LL2XrAuDWlqZ-XSeQ_572qk-63-UzpBA2tGggBaXoYv2cK1UCIC5sirVJUy",
+}
